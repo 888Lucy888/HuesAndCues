6671550c0c88d05be67d38719421cc63ad7502aa
888Lucy888 <chuggaaconroy888@gmail.com>
2021-04-10 12:15:55 -0500

    .

--- HuesAndCuesProject/src/huesandcuesproject/Runner.java
+++ HuesAndCuesProject/src/huesandcuesproject/Runner.java
@@ -45,9 +45,11 @@ public class Runner extends Application {
             iPlayers++;
             players.add(activePlayer);
         }
+        
+        for(int turn = 0; turn<nOfPlayers; turn++){}
         activePlayer = players.get(0);
         
-        userInput.askHint();
+        //userInput.askHint();
         
         GameLayout game = new GameLayout(nOfPlayers);
         
@@ -60,11 +62,13 @@ public class Runner extends Application {
             }
         }
         Collections.shuffle(shuffledArrayList);
+        
         //Create cards, index increases by 4:
         Card tempCard = new Card(shuffledArrayList);
         //MailSender.sendHTML("chuggaaconroy888@gmail.com", new Card(shuffledArrayList).toHTML());
         //MailSender.sendHTML("crlvlz0215@gmail.com", new Card(shuffledArrayList).toHTML());
         
+        userInput.askHint();
         
         Scene scene = new Scene(game, length, height);
         
